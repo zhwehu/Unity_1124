ac498c1f3228185a3a3955ebbb3a0f8b3f39cee4
zhwehu <74964516+zhwehu@users.noreply.github.com>
2020-12-17 21:31:06 +0800

    生成俄羅斯方塊+基本程式都完成

---
+++ 2D_game/Assets/Scripts/API_Notstatic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API_Notstatic : MonoBehaviour
+{
+    //使用一般屬性
+    //類型 欄位名稱
+    public Transform trA;
+    public Transform trB;
+    public GameObject myObject;
+    public Transform myTra;
+  
+
+    private void Start()
+    {
+        //★★★一般屬性★★★
+        //【取得】
+        //語法 : 類型欄位名稱 的 一般屬性
+        print(" 物件 A 的座標 : " + trA.position);
+
+        //【設定】
+        //語法 : //語法 : 類型欄位名稱 的 一般屬性 = 對應的值;
+        trB.position = new Vector3(1, 5, 3);
+
+        print("我的物件圖層為 : " + myObject.layer);
+        myObject.layer = 4;
+    }
+
+    //一秒執行60次事件
+    private void Update()
+    {
+        //一般方法
+        //【使用】
+        //語法 : 類型欄位名稱 的 方法(對應的參數)
+        myTra.Rotate(0, 0, 1);
+        myTra.Translate(1, 0, 0);
+    }
+}
--- 2D_game/Assets/Scripts/TetrisManager.cs
+++ 2D_game/Assets/Scripts/TetrisManager.cs
@@ -20,28 +20,62 @@ public class TetrisManager : MonoBehaviour
     public AudioClip soundMove;
     public AudioClip soundClear;
     public AudioClip soundLose;
+    [Header("下一個俄羅斯方塊區域")]
+    public Transform traNextArea;
+    [Header("畫布")]
+    public Transform traCanvas;
     #endregion
 
     #region 事件
+    /// <summary>
+    /// 下一顆俄羅斯方塊編號
+    /// </summary>
+    public int indexNext;
 
-    //private void Start()
-    //{
-    //Generate_Tetris();
-    // int add = addScore();
-    // print("傳回整數: "+ add);
-    //time();
-    //over();
-    //reture();
-    //exit();
-
-    //}
+    private void Start()
+    {
+        RondomTetris();
+    }
     #endregion
 
     #region 方法
 
-    private void SpawnTetris()
+    /// <summary>
+    /// 生成俄羅斯方塊
+    /// 1.隨機顯示一個下一顆俄羅斯方塊0-8
+    /// </summary>
+    private void RondomTetris()
     {
-        
+        //下一顆編號 = 隨機 的 範圍(最小,最大)  --> 整數不會等於最大值
+        indexNext = Random.Range(0, 9);
+        //下一顆俄羅斯方塊區域 . 取得子物件(子物件編號).轉為遊戲物件.啟動設定(顯示)
+        traNextArea.GetChild(indexNext).gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// 開始遊戲
+    /// 1.生成俄羅斯方塊要放在正確的位置
+    /// 2.上一次俄羅斯方塊隱藏
+    /// 3.隨機取得下一個
+    /// </summary>
+    public void StartGame()
+    {
+        //1.生成俄羅斯方塊要放在正確的位置
+        //保持上一次的俄羅斯方塊
+        GameObject tetris = traNextArea.GetChild(indexNext).gameObject;
+        //目前俄羅斯方塊 = 生成物件(物件, 父物件)
+        GameObject current = Instantiate(tetris,traCanvas);
+        //GetComponent<任何元件>()
+        //<T>泛型 - 指的是所有類型
+        //目前俄羅斯方塊 . 取得元件<介面變形>().座標 = 二維向量
+        current.GetComponent<RectTransform>().anchoredPosition = new Vector2(0,620);
+        //current.GetComponent<RectTransform>().Scale = new Vector3(0.3f,0.3f,0.3f);
+
+        // 2.上一次俄羅斯方塊隱藏
+        tetris.SetActive(false);
+        // 3.隨機取得下一個
+        RondomTetris();
+
     }
 
     public void addScore(int add)
